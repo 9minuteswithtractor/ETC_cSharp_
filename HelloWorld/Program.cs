10d29cc5ad004a3ff76572b53306e41c76c064f8
Peter <>
2023-11-12 10:50:21 +0200

    newFeat_comments

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace HelloWorld
+﻿using System.Text.Json;
+
+namespace HelloWorld
 {
     internal class Program
     {
+
         static void Main()
         {
+            // This is a LINE COMMENT
+
+            /*
+             *  This is
+             *     A
+             *     BLOCK COMMENT
+             */
+            
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Heisan, developer!");
             Console.Write("Please enter Your name: ");
