1dab4e710694b127b88af04e6dd63c0c87a814ab
Peter <>
2023-11-12 08:33:54 +0200

    ETC_cSharp_01_session

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Heisan, developer!");
+            Console.Write("Please enter Your name: ");
+            string userName = Console.ReadLine();
+
+            GreetUser(userName);
+            Console.ReadKey();
+        }
+
+        static void GreetUser(string userName)
+        {
+            Console.WriteLine($"Hello and Welcome to this C# course, {userName}!");
+        }
+    }
+}
