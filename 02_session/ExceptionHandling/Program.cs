62de4665961064b10bca4aec4cf81cf56e5d6553
Peter <>
2023-11-13 21:25:41 +0200

    Exception handling

---
+++ 02_session/ExceptionHandling/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+
+namespace ExceptionHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DemoTryCatch();
+        }
+
+        private static void DemoTryCatch()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Enter Your age: ");
+                    int userAge = int.Parse(Console.ReadLine());
+
+
+                    Console.WriteLine("\nAwesome!");
+                    Console.WriteLine($"I'm too {userAge}!");
+                    break;
+
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Please enter the right data type! ");
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+           
+
+            Console.ReadKey();
+        }
+    }
+}
