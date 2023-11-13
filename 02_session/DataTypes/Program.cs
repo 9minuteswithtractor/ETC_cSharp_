3857e45f53a466c2dd780830275e6ac6a7094ff0
Peter <>
2023-11-13 14:07:11 +0200

    upd : GC and errHandling implemented

--- 02_session/DataTypes/Program.cs
+++ 02_session/DataTypes/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace DataTpypes
 {
@@ -19,21 +20,58 @@ namespace DataTpypes
              *  long
              * 
              */
-
             Console.WriteLine("DataTypes:");
+
+
+            // TODO: strings
+            StringDataType();
+
+
+            // TODO: int
+            IntDataType();
+
+
+            //TODO: bool
+            BoolDataType();
+
+
+            //TODO: char
+            CharDataType();
+
+
+
+            Console.ReadKey();
+        }
+
+        private static void StringDataType()
+        {
             Console.BackgroundColor = ConsoleColor.White;
             Console.ForegroundColor = ConsoleColor.Black;
             Console.WriteLine("01 - string");
 
-
             Console.BackgroundColor = ConsoleColor.Black;
             Console.ForegroundColor = ConsoleColor.White;
             Console.Write("Enter Your name: ");
-            string userName = Console.ReadLine();
 
+            try
+            {
+            string userName = Console.ReadLine();
             Console.WriteLine("Hi and nice to meet You {0}!", userName);
             Console.WriteLine("Your name is type : {0}", userName.GetType());
+                GC.Collect();
 
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+
+            // TODO : freeing RAM memory
+
+        }
+
+        private static void IntDataType()
+        {
             Console.BackgroundColor = ConsoleColor.White;
             Console.ForegroundColor = ConsoleColor.Black;
             Console.WriteLine("02 - int");
@@ -42,11 +80,77 @@ namespace DataTpypes
             Console.BackgroundColor = ConsoleColor.Black;
             Console.ForegroundColor = ConsoleColor.White;
             Console.Write("Enter Your age: ");
-            int userAge = int.Parse(Console.ReadLine());
-            Console.WriteLine($"No way! I'm also {userAge} and my name is {userName}");
-            Console.WriteLine("You age type is : {0}", userAge.GetType());
+            // TODO: int.Parse
 
-            Console.ReadKey();
+            try
+            {
+            int? userAge = int.Parse(Console.ReadLine());
+            Console.WriteLine($"No way! I'm also {userAge}");
+            Console.WriteLine("You age type is : {0}", userAge.GetType());
+            GC.Collect();
+
+            } catch (Exception ex)
+            {
+                Console.WriteLine("Ivalid data type! Please enter number as Your age!");
+            }
+            ////////////////////////////////////////////////
+
+            
+        }
+
+        private static void BoolDataType()
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("03 - bool");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.Write("Do You have driving license: ");
+
+            try
+            {
+            string userInput = Console.ReadLine();
+            bool? hasLicense = userInput.ToLower().StartsWith("y") ? true : false;
+            Console.WriteLine($"User has driving license : {hasLicense}");
+            Console.WriteLine("The data type of hasLicense variable is : {0}", hasLicense.GetType());
+
+                // TODO : Garbage Collection
+                GC.Collect();
+            } catch (Exception ex)
+            {
+                Console.WriteLine("Please enter Yes or No ...");
+            }
+
+            // TODO: ternary operator 
+            ////////////////////////////////////////////////////////////////////////
+
+        }
+
+        private static void CharDataType()
+        {
+
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("04 - char");
+
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.Write("What size T-shirt You usally wear?(S,M,L): ");
+
+            try
+            {
+                char tShirtSize = char.Parse(Console.ReadLine());
+                Console.WriteLine("Nice! Your are wearing size: {0}", tShirtSize);
+
+
+            }
+            catch
+            {
+                Console.WriteLine("Input must be one char long");
+            }
         }
     }
 }
