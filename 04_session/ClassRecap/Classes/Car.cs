4f4bea2237e7e2e76fa16446dc04fb66666e5808
Peter <>
2023-11-21 16:07:20 +0200

    ClassRecap

---
+++ 04_session/ClassRecap/Classes/Car.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRecap.Classes
+{
+    internal class Car
+    {
+    }
+}
